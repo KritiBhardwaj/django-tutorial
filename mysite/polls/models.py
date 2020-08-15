d06f06e61e01a095c1ffd2dfa98638039128702b
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-08-15 17:58:07 +1000

    Django Week 7 - Session 1 Tutorial Part 1 and Part2 changes

---
+++ mysite/polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+#this will make a table called question in the db
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    #could be used for example to sort questions by date
+    pub_date = models.DateTimeField()
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.question_text
+
+#corresponds to a question, will be a table in the db
+class Choice(models.Model):
+    question = models.ForeignKey(
+        Question, 
+        on_delete=models.CASCADE
+    )
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
